fc754550204f15af4c085cb6034b0d44a16e3df2
Matt Bradshaw <matthewloydbradshaw@gmail.com>
2021-01-26 16:53:20 +0000

    here comes MIDI

--- experiments/polymodteensy/ModuleVCO.cpp
+++ experiments/polymodteensy/ModuleVCO.cpp
@@ -3,7 +3,7 @@
 
 ModuleVCO::ModuleVCO() {
   for(byte i=0; i<MAX_POLYPHONY; i++) {
-    _osc[i].begin(0.2, 22.5*pow(2,i), WAVEFORM_SAWTOOTH);
+    _osc[i].begin(0.2, 22.5*pow(2,i/2.0), WAVEFORM_SAWTOOTH);
     //_modCables[i] = new AudioConnection(freqModIn.amplifiers[i], 0, _osc[i], 0);
     _modCables[i] = new AudioConnection(_freqControl.dc, 0, _osc[i], 0);
     _oscCables[i] = new AudioConnection(_osc[i], 0, audioOut.amplifiers[i], 0);
--- experiments/polymodteensy/polymodteensy.ino
+++ experiments/polymodteensy/polymodteensy.ino
@@ -3,6 +3,9 @@
 #include <SPI.h>
 #include <SD.h>
 #include <SerialFlash.h>
+#include <MIDI.h>
+
+MIDI_CREATE_INSTANCE(HardwareSerial, Serial1, MIDI);
 
 AudioControlSGTL5000     sgtl5000_1;
 
@@ -63,12 +66,40 @@ void setup() {
   }
 
   Serial.begin(9600);
+  MIDI.begin(MIDI_CHANNEL_OMNI);
 }
 
 bool firstLoop = true;
 byte loopsWithDifference = 0;
 void loop() {
   moduleVCO.update();
+
+  int note, velocity, channel, d1, d2;
+  if (MIDI.read()) {
+    byte type = MIDI.getType();
+    switch (type) {
+      case midi::NoteOn:
+        note = MIDI.getData1();
+        velocity = MIDI.getData2();
+        channel = MIDI.getChannel();
+        if (velocity > 0) {
+          Serial.println(String("Note On:  ch=") + channel + ", note=" + note + ", velocity=" + velocity);
+        } else {
+          Serial.println(String("Note Off: ch=") + channel + ", note=" + note);
+        }
+        break;
+      case midi::NoteOff:
+        note = MIDI.getData1();
+        velocity = MIDI.getData2();
+        channel = MIDI.getChannel();
+        Serial.println(String("Note Off: ch=") + channel + ", note=" + note + ", velocity=" + velocity);
+        break;
+      default:
+        d1 = MIDI.getData1();
+        d2 = MIDI.getData2();
+        Serial.println(String("Message, type=") + type + ", data = " + d1 + " " + d2);
+    }
+  }
   
   byte b,m,c,n;
   for(n=0; n<NUM_CHANNELS; n++) {
